10880d49715677c42a0bfa347415b4e7ae529ab8
Francis O. Makokha <francismakokha@hotmail.com>
2015-04-08 02:50:12 +0200

    Change password done

--- user/change_password_action.php
+++ user/change_password_action.php
@@ -1,2 +1,41 @@
 <?php 
+echo "En attente...";
+if (isset($_POST['password_new']) AND isset($_POST['password']) AND isset($_POST['password_verif'])) {
+	$password_new= $_POST['password_new'];
+	$password_verif= $_POST['password_verif'];
+	$password= $_POST['password'];
+	$userID= $_SESSION['userID'];
+	$email= $_SESSION['email'];
+	
+	//print_r($userID);
+	if($password_new == $password_verif){
+	$query_password_check= "SELECT * FROM users_noyau WHERE id=$userID AND password='$password'";
+	$db_connect= db_connect();
+	$password_check= $db_connect->query($query_password_check);
+
+	if (mysqli_num_rows($password_check) == 1) {
+		$query_update_db= "UPDATE users_noyau SET password='$password_new' WHERE id=$userID";
+		$update_username= $db_connect->query($query_update_db);
+			if($update_username){
+				//echo "success4";
+				$emailfrom="notify@franmako.be";
+				$from="From:";
+				$from .= $emailfrom;
+				$sujet= "Mot de passe changé!";
+				$contenu=
+				'Bonjour,
+ 
+				Le mot de passe associé à cet adresse e-mail vient d\'être changé!
+ 				---------------
+				Ceci est un mail automatique, Merci de ne pas y répondre. Veuillez contacter l\'admin si ce changement ne vient pas de vous.';
+				$to=$email;
+				$sent = mail($to,$sujet,$contenu,$from);
+			
+				echo '<p> Le mot de passe a été changé avec succès. Reconnectez vous pour voir le changement. </p>';
+				}
+	}else {
+		echo "Les champs requis ne sont pas valides!";
+	}
+	}
+}
 ?>
--- user/change_password_form.php
+++ user/change_password_form.php
@@ -1,7 +1,7 @@
 <h3> Modifier votre mot de passe</h3>
 <form method="post" action="index.php?rq=password_modify" name="loginform" >
 	Nouveau mot de passe: <input type="password" name="password_new"/> </br>
-	Répéter nouveau mot de passe: <input type="password" name="password_new"/></br>
-    Mot de passe : <input type="password" name="password" id="password" /></br>
-    <p><input type="submit" name="login" id="login" value="Connexion" /></p>
+	Répéter nouveau mot de passe: <input type="password" name="password_verif"/></br>
+    Mot de passe : <input type="password" name="password"  /></br>
+    <p><input type="submit" name="login" id="login" value="Changer de mot de passe" /></p>
 </form>
